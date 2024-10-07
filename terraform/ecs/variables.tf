2fa915fe77a69254ba5108364741bfd778317634
Gabriel Martins <gabriel.hamartins@gmail.com>
2024-10-07 05:04:47 -0300

    Added terraform specs and updated README.

---
+++ terraform/ecs/variables.tf
@@ -0,0 +1,45 @@
+variable "aws_access_key" {
+  type        = string
+  description = "use env aws keys"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "use env aws keys"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "desired_capacity" {
+  description = "desired number of running nodes"
+  default     = 3
+}
+
+variable "container_port" {
+  default = "3000"
+}
+
+variable "image_url" {
+  default = "your-account.dkr.ecr.us-east-1.amazonaws.com/my-app-container:latest"
+}
+
+variable "memory" {
+  default = "512"
+}
+
+variable "cpu" {
+  default = "256"
+}
+
+variable "cluster_name" {
+  default = "my-cluster"
+}
+
+variable "cluster_task" {
+  default = "my-first-task"
+}
+variable "cluster_service" {
+  default = "my-first-service"
+}
